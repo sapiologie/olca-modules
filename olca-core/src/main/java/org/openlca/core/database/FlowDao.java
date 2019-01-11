201ee4ef830f72af2398218479443ee3c40f90b5
msrocka <michael.srocka@gmail.com>
2019-01-11 09:23:20 +0100

    Can we improve the descriptor handling?
    Descriptors are used in
    
    * the navigation,
    * the result views,
    * reference and identifiy things in data exchange
    
    For the first two points, we want to show the most important
    information of a data set. For the last point, we want to
    identifiy a dataset in both directions: import & export
    
    ## id, refId, lastChange, version, name, description, modelType
    All these fields should stay in the descriptors but we may can
    simplify the query protocoll a bit here. Also, performance tests
    would be good for the queries...
    
    ## Categories
    The navigation and filtering by category is important for openLCA.
    This is why storing the category ID in the descriptor probably is
    a good idea. But for the display we often need to show (a part) of
    the category path. Instead of building this path always dynamically
    we may should store it in a field `categoryPath::String[]` of the
    descriptor.
    
    ## Other fields
    We should store them directly as strings not via IDs; e.g. flow units,
    location codes --> we may can then drop the EntityCache? This would
    probably simplify the API in a lot of places
    
    ## The descriptor protocoll in the DAOs
    
    ### RootEntityDao
    * getDescriptors()
    * getDescriptor(long id)
    * getDescriptors(Set<Long> ids)
    * getDescriptorForRefId(String refId)
    * getDescriptorsForRefIds(Set<String> refIds)
    
    ### CategorizedEntityDao
    * getDescriptors(Optional<Category> category)
    
    => more?

--- olca-core/src/main/java/org/openlca/core/database/FlowDao.java
+++ olca-core/src/main/java/org/openlca/core/database/FlowDao.java
@@ -1,9 +1,11 @@
 package org.openlca.core.database;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,11 +13,79 @@ import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowType;
 import org.openlca.core.model.descriptors.FlowDescriptor;
 
+import gnu.trove.map.hash.TLongObjectHashMap;
+
 public class FlowDao extends CategorizedEntityDao<Flow, FlowDescriptor> {
 
 	public FlowDao(IDatabase database) {
 		super(Flow.class, FlowDescriptor.class, database);
 	}
+
+	// start: new descriptor implementation
+
+	@Override
+	public List<FlowDescriptor> getDescriptors() {
+		String sql = "SELECT id, ref_id, name, description, version, "
+				+ "last_change, f_category, flow_type, f_location, "
+				+ "f_reference_flow_property FROM tbl_flows";
+
+		ArrayList<FlowDescriptor> list = new ArrayList<>();
+		TLongObjectHashMap<String> locations = getLocationCodes();
+		try {
+			NativeSql.on(database).query(sql, r -> {
+				FlowDescriptor d = new FlowDescriptor();
+				d.id = r.getLong(1);
+				d.refId = r.getString(2);
+				d.name = r.getString(3);
+				d.description = r.getString(4);
+				d.version = r.getLong(5);
+				d.lastChange = r.getLong(6);
+				d.category = r.getLong(7);
+				if (r.wasNull()) {
+					d.category = null;
+				}
+				String fType = r.getString(8);
+				if (fType != null) {
+					d.flowType = FlowType.valueOf(fType);
+				}
+				long locationID = r.getLong(9);
+				if (!r.wasNull()) {
+					d.location = locations.get(locationID);
+				}
+
+				return true;
+			});
+		} catch (Exception e) {
+			log.error("Failed to load descriptors", e);
+		}
+
+		return list;
+	}
+
+	// move this as package private method to the location DAO
+	private TLongObjectHashMap<String> getLocationCodes() {
+		String sql = "SELECT id, code FROM tbl_locations";
+		TLongObjectHashMap<String> m = new TLongObjectHashMap<>();
+		try {
+			NativeSql.on(database).query(sql, r -> {
+				m.put(r.getLong(1), r.getString(2));
+				return true;
+			});
+		} catch (Exception e) {
+			log.error("Failed to query location codes", e);
+		}
+		return m;
+	}
+
+	/**
+	 * Returns a map that maps the IDs of the flow properties in the database to
+	 * the respective reference units.
+	 */
+	private TLongObjectHashMap<String> getRefUnits() {
+		return null;
+	}
+
+	// end: new descriptor implementation
 
 	@Override
 	protected String[] getDescriptorFields() {
@@ -70,8 +140,10 @@ public class FlowDao extends CategorizedEntityDao<Flow, FlowDescriptor> {
 	public Set<Long> getReplacementCandidates(long flowId, FlowType type) {
 		Set<Long> ids = new HashSet<>();
 		String query = "SELECT DISTINCT f_flow FROM tbl_flow_property_factors WHERE f_flow_property IN "
-				+ "(SELECT f_flow_property FROM tbl_flow_property_factors WHERE f_flow = " + flowId + ") "
-				+ "AND f_flow IN (SELECT DISTINCT id FROM tbl_flows WHERE flow_type = '" + type.name() + "')";
+				+ "(SELECT f_flow_property FROM tbl_flow_property_factors WHERE f_flow = "
+				+ flowId + ") "
+				+ "AND f_flow IN (SELECT DISTINCT id FROM tbl_flows WHERE flow_type = '"
+				+ type.name() + "')";
 		try {
 			NativeSql.on(database).query(query, (rs) -> {
 				ids.add(rs.getLong("f_flow"));
@@ -80,26 +152,33 @@ public class FlowDao extends CategorizedEntityDao<Flow, FlowDescriptor> {
 			ids.remove(flowId);
 			return ids;
 		} catch (Exception e) {
-			DatabaseException.logAndThrow(log, "failed to load replacement candidate flows for " + flowId, e);
+			DatabaseException.logAndThrow(log,
+					"failed to load replacement candidate flows for " + flowId,
+					e);
 			return Collections.emptySet();
 		}
 	}
 
-	public void replace(long oldId, long newId, boolean excludeExchangesWithProviders) {
+	public void replace(long oldId, long newId,
+			boolean excludeExchangesWithProviders) {
 		try {
 			String statement = null;
 			if (excludeExchangesWithProviders) {
 				statement = "UPDATE tbl_exchanges SET f_flow = " + newId + " "
-						+ "WHERE f_flow = " + oldId + " AND f_default_provider IS NULL";
+						+ "WHERE f_flow = " + oldId
+						+ " AND f_default_provider IS NULL";
 			} else {
-				statement = "UPDATE tbl_exchanges SET f_flow = " + newId + ", f_default_provider = null "
+				statement = "UPDATE tbl_exchanges SET f_flow = " + newId
+						+ ", f_default_provider = null "
 						+ "WHERE f_flow = " + oldId;
 			}
 			NativeSql.on(database).runUpdate(statement);
-			statement = "UPDATE tbl_impact_factors SET f_flow = " + newId + " WHERE f_flow = " + oldId;
+			statement = "UPDATE tbl_impact_factors SET f_flow = " + newId
+					+ " WHERE f_flow = " + oldId;
 			NativeSql.on(database).runUpdate(statement);
 		} catch (Exception e) {
-			DatabaseException.logAndThrow(log, "failed to replace flow " + oldId + " with " + newId, e);
+			DatabaseException.logAndThrow(log,
+					"failed to replace flow " + oldId + " with " + newId, e);
 		}
 	}
 
@@ -114,7 +193,8 @@ public class FlowDao extends CategorizedEntityDao<Flow, FlowDescriptor> {
 			});
 			return ids;
 		} catch (Exception e) {
-			DatabaseException.logAndThrow(log, "failed to load processes for flow " + flowId, e);
+			DatabaseException.logAndThrow(log,
+					"failed to load processes for flow " + flowId, e);
 			return Collections.emptySet();
 		}
 	}
@@ -132,7 +212,8 @@ public class FlowDao extends CategorizedEntityDao<Flow, FlowDescriptor> {
 		query.append("SELECT id, f_reference_flow_property FROM tbl_flows ");
 		query.append("WHERE id IN " + asSqlList(ids));
 		query.append(" AND f_reference_flow_property IN ");
-		query.append("(SELECT f_flow_property FROM tbl_flow_property_factors WHERE tbl_flows.id = f_flow)");
+		query.append(
+				"(SELECT f_flow_property FROM tbl_flow_property_factors WHERE tbl_flows.id = f_flow)");
 		Map<Long, Boolean> result = new HashMap<>();
 		for (long id : ids)
 			result.put(id, false);
--- olca-core/src/main/java/org/openlca/core/model/descriptors/FlowDescriptor.java
+++ olca-core/src/main/java/org/openlca/core/model/descriptors/FlowDescriptor.java
@@ -5,9 +5,22 @@ import org.openlca.core.model.ModelType;
 
 public class FlowDescriptor extends CategorizedDescriptor {
 
-	public Long location;
+	/**
+	 * The name or code of the location of the flow. Typically, this is only
+	 * used for product flows in databases like ecoinvent.
+	 */
+	public String location;
+
+	/**
+	 * The specific flow type (product, waste, or elementary flow).
+	 */
 	public FlowType flowType;
-	public long refFlowPropertyId;
+
+	/**
+	 * The reference unit of the flow (useful for the display of calculation
+	 * results with flow meta-data).
+	 */
+	public String unit;
 
 	public FlowDescriptor() {
 		this.type = ModelType.FLOW;
